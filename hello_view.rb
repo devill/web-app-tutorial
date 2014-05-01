9e37af8c1cbe7b775077f6804af978d7d885fdb6
devill <devillmeister@gmail.com>
2014-05-01 09:39:10 +0200

    The first refactoring

--- hello_view.rb
+++ hello_view.rb
@@ -1,11 +1,9 @@
 require 'mustache'
 
 class HelloView < Mustache
+  attr_reader :user_name
+
   def initialize(user_name = 'World')
     @user_name = user_name
-  end
-
-  def user_name
-    @user_name
   end
 end
